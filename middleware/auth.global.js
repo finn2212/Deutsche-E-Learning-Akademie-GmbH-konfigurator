3870b289b76fe3f417023489477aa27f0d7d299a
finn2212 <stolle.finn@googlemail.com>
2024-11-07 10:49:33 +0200

    role fix

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -8,21 +8,21 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   const { userRole, fetchUserRole } = useAuth(); 
 
-  await fetchUserRole();
+  // await fetchUserRole();
 
-  // Define dynamic roles based on route path
-  const roleMap = {
-    '/start': [Roles.SUPER_ADMIN, Roles.MODERATOR,Roles.ORGANIZATION_OWNER, Roles.ORGANIZATION_WORKER],
-    '/find-anbieter': Roles.SUPER_ADMIN,  // Ensure correct route with leading slash
-  };
+  // // Define dynamic roles based on route path
+  // const roleMap = {
+  //   '/start': [Roles.SUPER_ADMIN, Roles.MODERATOR,Roles.ORGANIZATION_OWNER, Roles.ORGANIZATION_WORKER],
+  //   '/find-anbieter': Roles.SUPER_ADMIN,  // Ensure correct route with leading slash
+  // };
 
-  // Get the required role for the current route
-  const requiredRole = roleMap[to.path];
+  // // Get the required role for the current route
+  // const requiredRole = roleMap[to.path];
 
-  // If a role is required for this route, call the helper function with that role
-  if (requiredRole) {
-    await checkUserRole(userRole.value, requiredRole, (path, options = {}) => {
-      return $router.push(path, options);  // Use $router to navigate
-    });
-  }
+  // // If a role is required for this route, call the helper function with that role
+  // // if (requiredRole) {
+  //   await checkUserRole(userRole.value, requiredRole, (path, options = {}) => {
+  //     return $router.push(path, options);  // Use $router to navigate
+  //   });
+  // // }
 });
