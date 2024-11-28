fcad5b21d4eded37a7fcad568588d4c02cf67134
finn2212 <stolle.finn@googlemail.com>
2024-11-28 09:04:23 +0200

    feat: added coockie banner

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,5 +20,14 @@ export default {
   router: {
     middleware: ['auth', 'subdomain'] // Ensure your subdomain middleware is correctly referenced
   },
+  head: {
+    script: [
+      {
+        src: "https://app.getterms.io/cookie-consent/embed/58238475-1bb5-4745-8cd8-667f773c422b/en-us",
+        type: "text/javascript",
+        defer: true // Optional: Ensures the script is loaded after the DOM is parsed
+      }
+    ]
+  },
   devtools: true
 }
